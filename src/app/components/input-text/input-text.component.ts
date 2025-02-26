a73cdb0712eac4b035c61b2738de83ad0dbf66a2
alxrgdriguez <sralexred@gmail.com>
2025-02-26 20:13:00 +0100

    delete package icons

--- src/app/components/input-text/input-text.component.ts
+++ src/app/components/input-text/input-text.component.ts
@@ -1,21 +1,21 @@
-import { Component, Input } from '@angular/core';
-import { FormControl, ReactiveFormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
-import { UserSvgComponent } from "../icons/user-svg.component";
+// import { Component, Input } from '@angular/core';
+// import { FormControl, ReactiveFormsModule } from '@angular/forms';
+// import { CommonModule } from '@angular/common';
+// import { UserSvgComponent } from "../icons/user-svg.component";
 
-@Component({
-  selector: 'app-input-text',
-  templateUrl: './input-text.component.html',
-  imports: [CommonModule, ReactiveFormsModule, UserSvgComponent]
-})
-export class InputTextComponent {
-getErrorMessage() {
-throw new Error('Method not implemented.');
-}
-  @Input() id!: string;
-  @Input() type: string = 'text';
-  @Input() placeholder!: string;
-  @Input() icon?: string;
-  @Input() control!: FormControl;
-  @Input() iconComponent?: string;
-}
+// @Component({
+//   selector: 'app-input-text',
+//   templateUrl: './input-text.component.html',
+//   imports: [CommonModule, ReactiveFormsModule, UserSvgComponent]
+// })
+// export class InputTextComponent {
+// getErrorMessage() {
+// throw new Error('Method not implemented.');
+// }
+//   @Input() id!: string;
+//   @Input() type: string = 'text';
+//   @Input() placeholder!: string;
+//   @Input() icon?: string;
+//   @Input() control!: FormControl;
+//   @Input() iconComponent?: string;
+// }
