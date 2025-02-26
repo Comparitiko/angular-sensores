1b246cc00c127394e32210740f701fd81d8e041e
missymogara <monicagara97@gmail.com>
2025-02-26 17:59:06 +0100

    done with login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { UserService } from '../services/user.service';
 
 
 @Component({
@@ -9,5 +10,38 @@ import { ReactiveFormsModule } from '@angular/forms';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  loginForm!: FormGroup;
 
+  constructor(private readonly fb: FormBuilder private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.loginForm = this.initForm();
+  }
+
+  
+
+  onSubmit(): void {
+    if (this.loginForm.valid) {
+      const user = {
+        name: this.loginForm.get('name')?.value,
+        password: this.loginForm.get('password')?.value
+      };
+
+      this.userService.login(user).subscribe(
+        (response: any) => {
+          console.log('Login exitoso:', response);
+        },
+        (error: any) => {
+          console.error('Error en el login:', error);
+        }
+      );
+    }
+  }
+  
+  initForm(): FormGroup {
+    return this.fb.group({
+      name: ['', [Validators.required]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+    })
+  }
 }
