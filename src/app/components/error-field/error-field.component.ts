d42c2348eb78f59d0380f325b458ba8f1098bc51
Comparitiko <gabigcy@gmail.com>
2025-02-26 19:53:12 +0100

    Hecho componente error-field y mejorado login

---
+++ src/app/components/error-field/error-field.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-error-field',
+  imports: [],
+  templateUrl: './error-field.component.html'
+})
+export class ErrorFieldComponent {
+
+}
