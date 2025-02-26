8c21dbaea65a821aa8903bb42106a7ed63dae347
Comparitiko <gabigcy@gmail.com>
2025-02-26 17:15:17 +0100

    Haciendo rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { Error404Component } from './pages/error404/error404.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+  }
+  {
+    path: '**',
+    component: Error404Component,
+  },
+];
---
+++ src/app/pages/error404/error404.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-error404',
+  imports: [],
+  templateUrl: './error404.component.html',
+})
+export class Error404Component {}
