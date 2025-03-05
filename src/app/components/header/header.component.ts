30a24a7091d888591d8608f9c4e2fc94ca9055f6
missymogara <monicagara97@gmail.com>
2025-03-05 18:22:47 +0100

    logout in header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {RouterLink} from '@angular/router';
+import { UserService } from '@/app/services/user.service';
 
 @Component({
   selector: 'app-header',
@@ -9,5 +10,10 @@ import {RouterLink} from '@angular/router';
   templateUrl: './header.component.html'
 })
 export class HeaderComponent {
+  constructor(private userService: UserService) {}
+  
+  onLogout(): void {
+    this.userService.logout();
+  }
 
 }
