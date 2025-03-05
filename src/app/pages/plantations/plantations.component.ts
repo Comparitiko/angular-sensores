daaca2dbbfbd70d1d3fe12625d6f3ea9cd934a0e
missymogara <monicagara97@gmail.com>
2025-03-05 17:40:52 +0100

    doing plantations page

--- src/app/pages/plantations/plantations.component.ts
+++ src/app/pages/plantations/plantations.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from '@/app/components/footer/footer.component';
+import { HeaderComponent } from '@/app/components/header/header.component';
+import { Plantation } from  '@/app/interfaces/plantation.interface';
 
 @Component({
   selector: 'app-plantations',
-  imports: [],
+  imports: [FooterComponent, HeaderComponent],
   templateUrl: './plantations.component.html'
 })
 export class PlantationsComponent {
+
+  plantations: Plantation[] = [];
 
 }
