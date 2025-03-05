d032ec5c3275cf4c7a3ab69b2d2554c2ee030f5c
missymogara <monicagara97@gmail.com>
2025-03-05 17:54:12 +0100

    plantation ubication interface

--- src/app/interfaces/plantation.interface.ts
+++ src/app/interfaces/plantation.interface.ts
@@ -3,7 +3,7 @@ import { Sensor } from './sensor.interface';
 export interface Plantation {
   id: number;
   name: string;
-  ubicacion: string;
+  ubication: string;
   country: string;
   province: string;
   city: string;
