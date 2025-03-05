2dcb0adb302ba0fc85ac6d8dca0ae106b31423d5
AdrianPerez2207 <aperezsanchez441@gmail.com>
2025-03-05 17:22:43 +0100

    login terminado

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -30,7 +30,12 @@ export class LoginComponent {
     this.loginForm = this.initForm();
   }
 
-  onSubmit(): void {
+  /**
+   * Al hacer Submit, validamos el formulario y enviamos la solicitud HTTP POST a la API para autenticar al usuario.
+   * Si la autenticación es exitosa, guardamos el token y el nombre de usuario en `sessionStorage`.
+   * Si la autenticación falla, mostramos un mensaje de error.
+   */
+  async onSubmit() {
     if (this.loginForm.valid) {
       const user: User = {
         username: this.loginForm.get('name')?.value,
@@ -39,19 +44,24 @@ export class LoginComponent {
 
       this.isLoading.set(true);
 
-      this.userService
-        .login(user)
-        .then((response: any) => {
-          console.log('Login exitoso:', response);
-        })
-        .catch((error: any) => {
-          console.error('Error en el login:', error);
-          this.errorInLogin.set(true);
+      const response = await this.userService.login(user);
+
+      response.subscribe({
+        next: (response) => {
+          this.userService.setUsername(response.username);
+          this.userService.setToken(response.token);
+        },
+        error: () => {
           this.isLoading.set(false);
-        });
+          this.errorInLogin.set(true);
+        }
+      })
     }
   }
 
+  /**
+   * Inicializa el formulario de login y valida los campos requeridos.
+   */
   initForm(): FormGroup {
     return this.fb.group({
       name: ['', [Validators.required]],
