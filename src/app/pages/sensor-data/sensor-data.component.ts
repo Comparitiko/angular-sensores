e46c01b998c6bb828f07547e060df590a1317a56
Comparitiko <gabigcy@gmail.com>
2025-03-05 17:37:29 +0100

    Rutas terminadas

---
+++ src/app/pages/sensor-data/sensor-data.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sensor-data',
+  imports: [],
+  templateUrl: './sensor-data.component.html'
+})
+export class SensorDataComponent {
+
+}
